19ef9a21fadb698117b68125a7e60fde715028cc
Cclz <rex620620>
2017-03-30 15:48:42 +0800

    use layoutinflater __ 分 class 3_ 打勾不會消失

--- app/src/main/java/com/cclz/myapp_170330_03/Myadapter.java
+++ app/src/main/java/com/cclz/myapp_170330_03/Myadapter.java
@@ -2,9 +2,12 @@ package com.cclz.myapp_170330_03;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -21,9 +24,11 @@ public class MyAdapter extends BaseAdapter {
     ArrayList<String>data;
 
 //    public MyAdapter(Context c, String[] d, String d2[]){
+    public boolean chks[];
     public MyAdapter(Context c, ArrayList<String>d){
         context=c;
         data=d;
+        chks =new boolean[d.size()];
 //        data2=d2;
     }
 
@@ -46,15 +51,20 @@ public class MyAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        Log.d("MyView", "p:" + position);
         View v=((Activity)context).getLayoutInflater().inflate(R.layout.myitem, null);  // 自訂Layout
-//            TextView tv=new TextView(MainActivity.this);
         TextView tv=(TextView)v.findViewById(R.id.textView);    // 自訂 tv 大型字體
-//        TextView tv2=(TextView)v.findViewById(R.id.textView2);    // 自訂 tv 大型字體
-//        tv.setText(data[position]);
-//        tv2.setText(data2[position]);
+        CheckBox chk=(CheckBox)v.findViewById(R.id.checkBox);
+        chk.setChecked(chks[position]);
+        chk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                chks[position] = isChecked;
+            }
+        });
         tv.setText(data.get(position));
-//            return tv;
+
         return v;
     }
 }
