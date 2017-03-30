a6058a90528775ad5da16c347ff8d997e8a3c441
Cclz <rex620620>
2017-03-30 15:24:49 +0800

    use layoutinflater __ 分 class

--- app/src/main/java/com/cclz/myapp_170330_03/MainActivity.java
+++ app/src/main/java/com/cclz/myapp_170330_03/MainActivity.java
@@ -14,43 +14,16 @@ public class MainActivity extends AppCompatActivity {
     String data[]={"蘋果", "檸檬", "香蕉", "橘子"};
     String data2[]={"Apple", "Lemon", "Banana", "Orange"};
     ListView lv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lv=(ListView)findViewById(R.id.listView);
-        MyAdapter adapter=new MyAdapter();
+        MyAdapter adapter=new MyAdapter(MainActivity.this, data, data2);
         lv.setAdapter(adapter);
 
     }
 
-    class MyAdapter extends BaseAdapter{
 
-        @Override
-        public int getCount() {
-            return data.length;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View v=getLayoutInflater().inflate(R.layout.myitem, null);  // 自訂Layout
-//            TextView tv=new TextView(MainActivity.this);
-            TextView tv=(TextView)v.findViewById(R.id.textView);    // 自訂 tv 大型字體
-            TextView tv2=(TextView)v.findViewById(R.id.textView2);    // 自訂 tv 大型字體
-            tv.setText(data[position]);
-            tv2.setText(data2[position]);
-//            return tv;
-            return v;
-        }
-    }
 }
---
+++ app/src/main/java/com/cclz/myapp_170330_03/Myadapter.java
@@ -0,0 +1,51 @@
+package com.cclz.myapp_170330_03;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by YVTC on 2017/3/30.
+ */
+
+public class MyAdapter extends BaseAdapter {
+
+    Context context;
+    String data[];
+    String data2[];
+
+    public MyAdapter(Context c, String[] d, String d2[]){
+        context=c;
+        data=d;
+        data2=d2;
+    }
+    @Override
+    public int getCount() {
+        return data.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v=((Activity)context).getLayoutInflater().inflate(R.layout.myitem, null);  // 自訂Layout
+//            TextView tv=new TextView(MainActivity.this);
+        TextView tv=(TextView)v.findViewById(R.id.textView);    // 自訂 tv 大型字體
+        TextView tv2=(TextView)v.findViewById(R.id.textView2);    // 自訂 tv 大型字體
+        tv.setText(data[position]);
+        tv2.setText(data2[position]);
+//            return tv;
+        return v;
+    }
+}
